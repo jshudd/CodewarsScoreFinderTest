9b4d2d5e0e4a50a2d2ca0a3ec14c252a6f085c3c
Jeremy Huddleston <jshudd@gmail.com>
2023-09-18 21:44:32 -0500

    calling ReadCSV() works

--- CodewarsScoreFinderTest/Program.cs
+++ CodewarsScoreFinderTest/Program.cs
@@ -1,3 +1,16 @@
-﻿
-Console.WriteLine("Codewars Score Finder\n");
+﻿using CodewarsScoreFinderTest;
 
+Console.WriteLine("Codewars Score Finder\n");
+Console.WriteLine();
+
+// testing reading CSV file
+string relativeFilePath = @"ClassLists/CodewarsTest.csv";
+
+string csvPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, relativeFilePath);
+
+CSV.ReadCSV(csvPath);
+
+foreach (var user in User.UsersList)
+{
+    Console.WriteLine(user.UserName);
+}

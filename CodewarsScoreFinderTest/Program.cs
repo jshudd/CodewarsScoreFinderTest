062948a13e1f6780e883f4a60c627758ebdb0781
Jeremy Huddleston <jshudd@gmail.com>
2023-09-19 11:47:08 -0500

    added C#40.csv

--- CodewarsScoreFinderTest/Program.cs
+++ CodewarsScoreFinderTest/Program.cs
@@ -2,7 +2,7 @@
 
 Console.WriteLine("Codewars Score Finder\n");
 
-string relativeFilePath = @"ClassLists/CodewarsTest.csv";
+string relativeFilePath = @"ClassLists/C#40.csv";
 
 string csvPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, relativeFilePath);
 

26ff29c0b8bdfe30407454dfb8b71b0a1311b6ac
Jeremy Huddleston <jshudd@gmail.com>
2023-09-19 10:22:15 -0500

    removed comments

--- CodewarsScoreFinderTest/Program.cs
+++ CodewarsScoreFinderTest/Program.cs
@@ -2,24 +2,12 @@
 
 Console.WriteLine("Codewars Score Finder\n");
 
-// testing reading CSV file
 string relativeFilePath = @"ClassLists/CodewarsTest.csv";
 
 string csvPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, relativeFilePath);
 
 CSV.ReadCSV(csvPath);
 
-//foreach (var user in User.UsersList)
-//{
-//    user.JSON = API.CallAPI(user.UserName);
-//    user.ParseJSON();
-//}
-
 User.PopulateList();
-
-//foreach (var user in User.UsersList)
-//{
-//    Console.WriteLine($"{user.Name}, {user.UserName}, {user.Honor}");
-//}
 
 User.DisplayList();

b8e1e4fe0c9851abbfd3e2e272ec91d7c311ab77
Jeremy Huddleston <jshudd@gmail.com>
2023-09-18 20:54:47 -0500

    created Users class
    created Users class; added properties

---
+++ CodewarsScoreFinderTest/Users.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace CodewarsScoreFinderTest
+{
+	public class Users
+	{
+		public Users()
+		{
+		}
+
+		public string UserName { get; set; }
+		public string Name { get; set; }
+		public int Honor { get; set; }
+		public string JSON { get; set; }
+
+		public List<Users> UsersList { get; set; }
+	}
+}
+

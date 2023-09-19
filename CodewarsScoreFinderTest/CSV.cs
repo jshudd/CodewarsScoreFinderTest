b1cfb01f46850f56a3c158b9a6d7457e82cba834
Jeremy Huddleston <jshudd@gmail.com>
2023-09-19 17:06:30 -0500

    updated ReadCSV()

--- CodewarsScoreFinderTest/CSV.cs
+++ CodewarsScoreFinderTest/CSV.cs
@@ -4,16 +4,17 @@ namespace CodewarsScoreFinderTest
 	public static class CSV
 	{	
 		public static void ReadCSV(string path)
-		{
+		{            
             var lines = File.ReadAllLines(path);
 
 			foreach (var user in lines)
 			{
-				var newUser = new User();
+                var newUser = new User
+                {
+                    UserName = user
+                };
 
-				newUser.UserName = user;
-
-				User.UsersList.Add(newUser);
+                User.UsersList.Add(newUser);
 			}
         }
 	}

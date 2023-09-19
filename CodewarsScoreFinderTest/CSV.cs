36d91f1603e069e78f59595ed889167b4c5c1c0e
Jeremy Huddleston <jshudd@gmail.com>
2023-09-18 21:13:38 -0500

    updated CSV
    implemented ReadCSV()

--- CodewarsScoreFinderTest/CSV.cs
+++ CodewarsScoreFinderTest/CSV.cs
@@ -3,10 +3,21 @@ namespace CodewarsScoreFinderTest
 {
 	public static class CSV
 	{
+		const string csvPath = "~/ClassLists/CodewarsTest.csv";
+
 		public static void ReadCSV()
 		{
-			throw new NotImplementedException();
-		}
+            var lines = File.ReadAllLines(csvPath);
+
+			foreach (var user in lines)
+			{
+				var newUser = new User();
+
+				newUser.UserName = user;
+
+				User.UsersList.Add(newUser);
+			}
+        }
 	}
 }
 

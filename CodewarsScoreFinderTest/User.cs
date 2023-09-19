81b8e23316ffa79a2b00e545bcc9136817570dc2
Jeremy Huddleston <jshudd@gmail.com>
2023-09-18 22:08:51 -0500

    updated User class; implemented ParseJSON()

--- CodewarsScoreFinderTest/User.cs
+++ CodewarsScoreFinderTest/User.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Newtonsoft.Json.Linq;
+
 namespace CodewarsScoreFinderTest
 {
 	public class User
@@ -9,10 +11,17 @@ namespace CodewarsScoreFinderTest
 
 		public string UserName { get; set; }
 		public string Name { get; set; }
-		public int Honor { get; set; }
+		public string Honor { get; set; }
 		public string JSON { get; set; }
 
 		public static List<User> UsersList { get; set; } = new List<User>();
+
+		public void ParseJSON()
+		{
+			Name = JObject.Parse(JSON).GetValue("name").ToString();
+
+            Honor = JObject.Parse(JSON).GetValue("honor").ToString();
+        }
 	}
 }
 

ef1f3b8d189ff610b1332feb0754b0952ff1fac7
Jeremy Huddleston <jshudd@gmail.com>
2023-09-18 21:58:27 -0500

    implemented CallAPI()

--- CodewarsScoreFinderTest/API.cs
+++ CodewarsScoreFinderTest/API.cs
@@ -3,10 +3,14 @@ namespace CodewarsScoreFinderTest
 {
 	public static class API
 	{
-		public static void CallAPI()
+		public static string CallAPI(string userName)
 		{
-			throw new NotImplementedException();
-		}
+			var cwURL = $"https://www.codewars.com/api/v1/users/{userName}";
+
+			var client = new HttpClient();
+
+			return client.GetStringAsync(cwURL).Result;
+        }
 	}
 }
 

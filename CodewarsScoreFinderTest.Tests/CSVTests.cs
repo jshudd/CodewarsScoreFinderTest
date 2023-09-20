e6e791ae5e81939bae058237f55141c4f5b55b07
Jeremy Huddleston <jshudd@gmail.com>
2023-09-20 14:56:35 -0500

    created CreateCSVFile_Success test

--- CodewarsScoreFinderTest.Tests/CSVTests.cs
+++ CodewarsScoreFinderTest.Tests/CSVTests.cs
@@ -11,4 +11,22 @@ public class UnitTest1
 
         Assert.ThrowsAny<DirectoryNotFoundException>(() => CSV.ReadCSV(csvPath));
     }
+
+    [Fact]
+    public void CreateCSVFile_Success()
+    {
+        var userNames = new List<User>()
+        {
+            new User { UserName = "User1" },
+            new User { UserName = "User2"},
+            new User { UserName = "User3"}
+        };
+
+        CSV.CreateCSV(userNames);
+
+        string relativeFilePath = @"ClassLists/test.csv";
+        string csvPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, relativeFilePath);
+
+        Assert.NotNull(csvPath);
+    }
 }

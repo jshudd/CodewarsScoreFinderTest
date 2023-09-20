9ba98562e41c8a0be4c5c3dd7ae770b3eeafdb9e
Jeremy Huddleston <jshudd@gmail.com>
2023-09-20 16:20:44 -0500

    stubbed out methods to implement

--- CodewarsScoreFinderTest/CSV.cs
+++ CodewarsScoreFinderTest/CSV.cs
@@ -5,9 +5,25 @@ namespace CodewarsScoreFinderTest
 	{
 		public static void CreateCSV()
 		{
+            //call method to ask user to enter usernames
 			//call method to create filename for new file
-			//call method to ask user to enter usernames
+            //call method to generate CSV file
 
+            throw new NotImplementedException();
+		}
+
+		public static string EnterFileName()
+		{
+			throw new NotImplementedException();
+		}
+
+		public static List<User> EnterUserNames()
+		{
+			throw new NotImplementedException();
+		}
+
+		public static void GenerateCSV(string fileName, List<User> users)
+		{
 			throw new NotImplementedException();
 		}
 

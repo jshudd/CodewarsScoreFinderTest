2474b7e4186666b61fca54ea160a68564fe9d88c
Jeremy Huddleston <jshudd@gmail.com>
2023-09-20 13:43:23 -0500

    renamed test class; implemented CSVFileNotFound test

---
+++ CodewarsScoreFinderTest.Tests/CSVTests.cs
@@ -0,0 +1,14 @@
+﻿namespace CodewarsScoreFinderTest.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void CSVFileNotFound()
+    {
+        string relativeFilePath = @"ClassLists/C#4.csv";
+
+        string csvPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, relativeFilePath);
+
+        Assert.ThrowsAny<DirectoryNotFoundException>(() => CSV.ReadCSV(csvPath));
+    }
+}

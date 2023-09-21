bee7423f28376020255157ba005f5f5e65f58950
Jeremy Huddleston <jshudd@gmail.com>
2023-09-21 15:59:14 -0500

    created APITests

---
+++ CodewarsScoreFinderTest.Tests/APITests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace CodewarsScoreFinderTest.Tests
+{
+	public class APITests
+	{
+		[Theory]
+		[InlineData("jshudd")]
+        [InlineData("chipelmer")]
+        [InlineData("Bryantellius")]
+        [InlineData("TheMrChaptastic")]
+        [InlineData("mvdoyle")]
+        [InlineData("amoriss")]
+        [InlineData("CruzSanchez")]
+        [InlineData("whitstroup")]
+        public void APICallDoesReturnData_Success(string userName)
+		{
+			var result = API.CallAPI(userName);
+
+			Assert.NotNull(result);
+		}
+
+		[Theory]
+        [InlineData("jshuddabcdefg")]
+        public void APICallDoesNOTReturnData(string userName)
+		{
+            Assert.Throws<AggregateException>(() => API.CallAPI(userName));
+        }
+	}
+}
+

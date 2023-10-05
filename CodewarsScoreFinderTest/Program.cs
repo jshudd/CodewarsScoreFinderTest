6fd9018945bdb3a028259145d968951f55ed9ce0
Jeremy Huddleston <jshudd@gmail.com>
2023-10-05 15:56:41 -0500

    updated program file

--- CodewarsScoreFinderTest/Program.cs
+++ CodewarsScoreFinderTest/Program.cs
@@ -1,18 +1,20 @@
 ﻿using CodewarsScoreFinderTest;
 
+//string relativeFilePath = @"ClassLists/C#40.csv";
+////string relativeFilePath = @"ClassLists/CodewarsTest.csv";
+
+//string csvPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, relativeFilePath);
+
+//CSV.ReadCSV(csvPath);
+
+//User.PopulateList();
+
+////User.DisplayList();
+
+//Console.WriteLine("Score in Descending Order");
+
+//User.DisplayListHighScore();
+
+// New Implementation
 Console.WriteLine("Codewars Score Finder\n");
 
-string relativeFilePath = @"ClassLists/C#40.csv";
-//string relativeFilePath = @"ClassLists/CodewarsTest.csv";
-
-string csvPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, relativeFilePath);
-
-CSV.ReadCSV(csvPath);
-
-User.PopulateList();
-
-//User.DisplayList();
-
-Console.WriteLine("Score in Descending Order");
-
-User.DisplayListHighScore();

a238d35ce9ad7912cb75b4e6ff83b0d5949590f8
Jeremy Huddleston <jshudd@gmail.com>
2023-10-09 14:59:17 -0500

    updated UI window settings

--- CodewarsScoreFinderTest/Program.cs
+++ CodewarsScoreFinderTest/Program.cs
@@ -17,6 +17,15 @@
 
 // New Implementation
 
+// Set Background Color
+Console.BackgroundColor = ConsoleColor.Black;
+// Set Text Color
+Console.ForegroundColor = ConsoleColor.White;
+// Set Window Position (not suported on Mac)
+//Console.SetWindowPosition(Console.LargestWindowWidth / 2, Console.LargestWindowHeight / 2);
+// Set Window Size (not suported on Mac)
+//Console.SetWindowSize(500, 500);
+
 var isTrue = true;
 
 Console.WriteLine("Codewars Score Finder\n");

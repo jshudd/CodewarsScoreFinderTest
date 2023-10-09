32d1099f9019520628e1e7c542b035fd7ac84dd1
Jeremy Huddleston <jshudd@gmail.com>
2023-10-09 15:15:09 -0500

    removed unnecessary comments

--- CodewarsScoreFinderTest/Program.cs
+++ CodewarsScoreFinderTest/Program.cs
@@ -1,22 +1,5 @@
 ﻿using CodewarsScoreFinderTest;
 
-//string relativeFilePath = @"ClassLists/C#40.csv";
-////string relativeFilePath = @"ClassLists/CodewarsTest.csv";
-
-//string csvPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, relativeFilePath);
-
-//CSV.ReadCSV(csvPath);
-
-//User.PopulateList();
-
-////User.DisplayList();
-
-//Console.WriteLine("Score in Descending Order");
-
-//User.DisplayListHighScore();
-
-// New Implementation
-
 // Set Console Title
 Console.Title = "Codewars Score Finder";
 // Set Background Color
